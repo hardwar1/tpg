5e2ac3a6e8c3b9b27df5699284382cde7da2ddcc
hardwar1 <xapgbap1@ya.ru>
2020-03-25 11:47:16 +0300

    сделал адаптив

--- app/js/js-source/main.js
+++ app/js/js-source/main.js
@@ -1,8 +1,7 @@
 $(function () {
-  //весь код писать внутри что бы дом загрузился первым
 
-  $('.header__burger').on('click', function () {
-    $('.header__nav-inner').slideToggle();
+  $('.burger').on('click', function () {
+    $('.header__menu').slideToggle();
     $(this).toggleClass('burger__opened');
   });
 
@@ -12,23 +11,6 @@ $(function () {
     arrows: false
   });
 
-  // magnific-popap video youtube
-  $('.popup').magnificPopup({
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 400,
-    preloader: false,
-    iframe: {
-      patterns: {
-        youtube: {
-          index: 'youtube.com',
-          id: 'v=',
-          src: 'https://www.youtube.com/embed/%id%?rel=0&autoplay=1'
-        }
-      }
-    },
-  });
-  // magnific-popap video youtube
 
   new WOW().init();
 });
